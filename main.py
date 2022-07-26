30fd5b72fc0c63aec2d64586a9f9595872db4da7
Tomasz987 <tomek987125@gmail.com>
2022-07-26 21:56:07 +0200

    Create MainWindow view

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFrame, QVBoxLayout, QGridLayout
+from PyQt6.QtCore import Qt
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setGeometry(100, 100, 1200, 800)
+        self.setWindowTitle('I dont know what i do')
+
+        self.frame = QFrame()
+
+        self.main_layout = Q
+
+
+
